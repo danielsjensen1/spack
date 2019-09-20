d1a9210f8798fe5a507052abcca41a57edc53bd6
dsjense <dsjense@sandia.gov>
2019-09-20 15:00:35 -0600

    Update phonopy to use PIP download servers since it is no longer updated on sourceforge.

--- var/spack/repos/builtin/packages/py-phonopy/package.py
+++ var/spack/repos/builtin/packages/py-phonopy/package.py
@@ -10,9 +10,11 @@ class PyPhonopy(PythonPackage):
     """Phonopy is an open source package for phonon
     calculations at harmonic and quasi-harmonic levels."""
     homepage = "http://atztogo.github.io/phonopy/index.html"
-    url      = "http://sourceforge.net/projects/phonopy/files/phonopy/phonopy-1.10/phonopy-1.10.0.tar.gz"
+    #url      = "http://sourceforge.net/projects/phonopy/files/phonopy/phonopy-1.10/phonopy-1.10.0.tar.gz"
+    url      = "https://pypi.io/packages/source/p/phonopy/phonopy-2.3.2.tar.gz"
 
-    version('1.10.0', '973ed1bcea46e21b9bf747aab9061ff6')
+    version('2.3.2', 'd25458f9a0a244599ffd3385e17fb4341bc173131f21b6a57f0536969b376673')
+    #version('1.10.0', '973ed1bcea46e21b9bf747aab9061ff6')
 
     depends_on('py-numpy', type=('build', 'run'))
     depends_on('py-scipy', type=('build', 'run'))
